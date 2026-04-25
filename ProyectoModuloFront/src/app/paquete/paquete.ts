0c14e131229396c21d415141fc4a7152a3bbec92
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-04-25 14:06:52 -0500

    paquete
    paquete

---
+++ ProyectoModuloFront/src/app/paquete/paquete.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paquete',
+  standalone: false,
+  templateUrl: './paquete.html',
+  styleUrl: './paquete.css',
+})
+export class Paquete {}
