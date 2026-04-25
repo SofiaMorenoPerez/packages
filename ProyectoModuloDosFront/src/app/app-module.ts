492d5f75e5f5736be58db3e0d5bc659f671e3918
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-04-25 18:43:37 -0500

    lo que isabella / susan
    elimnado

--- ProyectoModuloDosFront/src/app/app-module.ts
+++ ProyectoModuloDosFront/src/app/app-module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Inicio } from './inicio/inicio';
-import { Models } from './models/models';
-import { Services } from './services/services';
 import { Paquete } from './paquete/paquete';
 import { Usuario } from './usuario/usuario';
 import { Empleado } from './empleado/empleado';
 
 @NgModule({
-  declarations: [App, Inicio, Models, Services, Paquete, Usuario, Empleado],
+  declarations: [App, Inicio, Paquete, Usuario, Empleado],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- ProyectoModuloDosFront/src/app/models/models.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-models',
-  standalone: false,
-  templateUrl: './models.html',
-  styleUrl: './models.css',
-})
-export class Models {}
