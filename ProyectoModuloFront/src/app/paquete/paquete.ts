e9dcd92e4b3937b5390a00362dc6f183da1503f7
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-04-25 18:13:20 -0500

    deleted
    borrar

--- ProyectoModuloFront/src/app/paquete/paquete.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-paquete',
-  standalone: false,
-  templateUrl: './paquete.html',
-  styleUrl: './paquete.css',
-})
-export class Paquete {}
