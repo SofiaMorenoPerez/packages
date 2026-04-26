15ab5b4a9683b40c69c4b55d1a831e96cd066afa
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-04-25 23:00:22 -0500

    Update app-routing-module.ts

--- ProyectoModuloDosFront/src/app/app-routing-module.ts
+++ ProyectoModuloDosFront/src/app/app-routing-module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
+import {Usuario} from './usuario/usuario';
 
-const routes: Routes = [{path: '', component: Inicio}];
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: Inicio },
+  { path: 'usuario', component: Usuario }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
