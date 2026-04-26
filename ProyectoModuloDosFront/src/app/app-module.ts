dc8daa761895778d3180ce9c55798850538127ae
imoreno1010 <imorenope@unbosque.edu.co>
2026-04-25 21:53:24 -0500

    admin
    empleadoadmin

--- ProyectoModuloDosFront/src/app/app-module.ts
+++ ProyectoModuloDosFront/src/app/app-module.ts
@@ -1,6 +1,8 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Inicio } from './inicio/inicio';
@@ -10,8 +12,16 @@ import { Empleado } from './empleado/empleado';
 
 @NgModule({
   declarations: [App, Inicio, Paquete, Usuario, Empleado],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideBrowserGlobalErrorListeners()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    provideAnimations(),
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
--- ProyectoModuloDosFront/src/app/services/admin.service.ts
+++ ProyectoModuloDosFront/src/app/services/admin.service.ts
@@ -1,6 +1,41 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EmpleadoAdminModel } from '../models/admin.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class AdminService {}
+export class EmpleadoAdminService {
+  private cliente = inject(HttpClient);
+  private readonly urlbase: string = 'http://localhost:8080';
+
+  getAll() {
+    return this.cliente.get<EmpleadoAdminModel[]>(
+      this.urlbase + '/administrativo/mostrartodo',
+      { observe: 'response' }
+    );
+  }
+
+  create(nombre: string, edad: number, fechaInicio: string, zonaAsignada: string) {
+    return this.cliente.post(
+      this.urlbase + `/administrativo/crearadministrativo?nombre=${nombre}&edad=${edad}&fechaInicio=${fechaInicio}&zonaAsignada=${zonaAsignada}`,
+      null,
+      { responseType: 'text' }
+    );
+  }
+
+  update(id: number, nombre: string, edad: number, fechaInicio: string, zonaAsignada: string) {
+    return this.cliente.put(
+      this.urlbase + `/administrativo/actualizar?id=${id}&nombre=${nombre}&edad=${edad}&fechaInicio=${fechaInicio}&zonaAsignada=${zonaAsignada}`,
+      null,
+      { responseType: 'text' }
+    );
+  }
+
+  delete(id: number) {
+    return this.cliente.delete(
+      this.urlbase + `/administrativo/eliminar?id=${id}`,
+      { responseType: 'text' }
+    );
+  }
+}
