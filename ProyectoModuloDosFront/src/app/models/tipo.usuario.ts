51e32518968dec705c6469a873b909b99e34c821
SofiaMorenoPerez <smorenoper@unbosque.edu.co>
2026-04-25 23:36:34 -0500

    okk
    cambio usuarios

---
+++ ProyectoModuloDosFront/src/app/models/tipo.usuario.ts
@@ -0,0 +1,5 @@
+export enum TipoUsuario {
+  NORMAL = 'NORMAL',
+  PREMIUM = 'PREMIUM',
+  CONCURRENTE = 'CONCURRENTE'
+}
--- ProyectoModuloDosFront/src/app/models/usuario.model.ts
+++ ProyectoModuloDosFront/src/app/models/usuario.model.ts
@@ -1,1 +1,11 @@
-export interface UsuarioModel {}
+import {TipoUsuario} from './tipo.usuario';
+
+interface UsuarioModel {
+  id: number;
+  nombre: string;
+  tipo: TipoUsuario;
+  tarifa: number;
+  ciudad: string;
+  direccion: string;
+  telefono: number;
+}
