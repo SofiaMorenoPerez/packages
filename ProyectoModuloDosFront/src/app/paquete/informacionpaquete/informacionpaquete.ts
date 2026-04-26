447925d17dc2ae5828271f44b079e8fe09366248
Suriza059 <suriza@unbosque.edu.co>
2026-04-25 23:16:18 -0500

    cambio componentes
    se quitaron los componentes de crear envio e info paquete

--- ProyectoModuloDosFront/src/app/paquete/informacionpaquete/informacionpaquete.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-informacionpaquete',
-  standalone: false,
-  templateUrl: './informacionpaquete.html',
-  styleUrl: './informacionpaquete.css',
-})
-export class Informacionpaquete {}
