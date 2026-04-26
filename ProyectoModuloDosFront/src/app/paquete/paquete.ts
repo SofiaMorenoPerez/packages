f2fae775c0f7fc5fa37a1883e0c408c528d27cd2
Suriza059 <suriza@unbosque.edu.co>
2026-04-26 00:03:34 -0500

    Union inicio paquete
    modifique el inicio para que direccione a paquete

--- ProyectoModuloDosFront/src/app/paquete/paquete.ts
+++ ProyectoModuloDosFront/src/app/paquete/paquete.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paquete',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './paquete.html',
   styleUrl: './paquete.css',
 })
-export class Paquete {}
+export class Paquete {
+
+  constructor(private router: Router) {}
+
+  irPaquete() {
+    this.router.navigate(['/paquete']);
+  }
+}
